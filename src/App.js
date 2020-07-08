fecff26e4afd05fb94321f0ab585c928c82d1f9e
Gabriel Vander <gabsvander@hotmail.com>
2020-07-08 19:49:51 -0300

    [#5] Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import ExampleApp from "./components/ExampleApp";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <ExampleApp/>
+          <BrowserRouter>
+            <Switch>
+                <Route exact path='/'>
+                    <ExampleApp/>
+                </Route>
+            </Switch>
+          </BrowserRouter>
       </div>
     </div>
   );
