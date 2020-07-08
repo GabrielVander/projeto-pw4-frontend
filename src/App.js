a6dce4578bece2dd71955e5707f95b71519d5cc5
Gabriel Vander <gabsvander@hotmail.com>
2020-07-08 20:16:13 -0300

    [#5] Further refactored the project's structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import './App.css';
-import ExampleApp from "./components/ExampleApp";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-          <BrowserRouter>
-            <Switch>
-                <Route exact path='/'>
-                    <ExampleApp/>
-                </Route>
-            </Switch>
-          </BrowserRouter>
-      </div>
-    </div>
+    <BrowserRouter>
+        <Switch>
+            <Route exact path='/'>
+                <MainPage/>
+            </Route>
+        </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/MainPage/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+import ExampleApp from "../../components/ExampleApp";
+
+function MainPage() {
+    return (
+        <div className="App">
+            <div className="App-header">
+                <ExampleApp/>
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
