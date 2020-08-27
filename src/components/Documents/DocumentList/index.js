10b44d68faf7c15c895167f50cdad10954597a6a
Gabriel Vander <gabsvander@hotmail.com>
2020-08-27 19:22:47 -0300

    [#24] Minor adjustments and bug fixes

--- src/components/Documents/DocumentList/index.js
+++ src/components/Documents/DocumentList/index.js
@@ -1,35 +1,44 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { useHistory } from 'react-router-dom';
 import ListGroup from 'react-bootstrap/ListGroup';
 import axios from 'axios';
 
 import { API_ENDPOINT } from '../../../const/';
+import Spinner from 'react-bootstrap/Spinner';
 
 function DocumentList() {
 
-	const [documents, setDocuments] = useState([]);
+	const [documents, setDocuments] = useState(null);
+	const [loading, setLoading] = useState(true);
 
-	axios.get(`${API_ENDPOINT}/documents`)
-		.then(response => {
-			setDocuments(response.data);
-		})
-		.catch(err => {
-			console.log(err);
-		});
+	useEffect(() => {
+		if(loading && documents == null) axios.get(`${API_ENDPOINT}/documents`)
+			.then(response => {
+				setDocuments(response.data);
+				setLoading(false);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	});
 
 	const history = useHistory();
 	
 	return (
 		<>
 			<h2 className='document-list-title'>All Documents</h2>
-			<ListGroup>
-				{documents.map(document => {
-					return <ListGroup.Item key={document.id} action onClick={() => history.push(`/documents/${document.id}`)}>
-						{document.title}
-					</ListGroup.Item>;
-				})
-				}
-			</ListGroup>
+			{loading ? 
+				<Spinner animation="border" role="status">
+					<span className="sr-only">Loading...</span>
+				</Spinner> 
+				: <ListGroup>
+					{documents.map(document => {
+						return <ListGroup.Item key={document._id} action onClick={() => history.push(`/documents/${document._id}`)}>
+							{document.title}
+						</ListGroup.Item>;
+					})
+					}
+				</ListGroup>}
 		</>
 	);
 }
--- src/pages/Documents/ViewDocumentPage/index.js
+++ src/pages/Documents/ViewDocumentPage/index.js
@@ -1,27 +1,37 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 import ViewDocument from '../../../components/Documents/ViewDocument';
-import { API_ENDPOINT } from '../../../const';
+import {API_ENDPOINT} from '../../../const';
 
 import './styles.css';
+import Document from '../../../model/Document';
 
 function ViewDocumentPage(props) {
 
 	const [document, setDocument] = useState(null);
 
-	axios.get(`${API_ENDPOINT}/documents/${props.match.params.documentId}`)
-		.then(response => {
-			setDocument(response.data);
-		})
-		.catch(err => {
-			console.log(err);
-		});
+	useEffect(() => {
+		if(document == null) axios.get(`${API_ENDPOINT}/documents/${props.match.params.documentId}`)
+			.then(response => {
+				setDocument(
+					new Document(
+						response.data._id,
+						response.data.title,
+						response.data.content
+					)
+				);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}
+	);
 
 	return (
 		<>
-			<ViewDocument document={document} />
+			{ document && <ViewDocument document={document} />}
 		</>
 	);
 }
