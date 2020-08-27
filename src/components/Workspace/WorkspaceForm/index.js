c44e29c2e8a2d2ee058350cd5f6772a17b04641d
felipe <felipe98_stz@hotmail.com>
2020-08-27 02:19:50 -0300

    adding, updating, selecting and deleting workspaces

---
+++ src/components/Workspace/WorkspaceForm/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './styles.css';
+import {IconContext} from 'react-icons';
+import {RiSave3Line, RiCloseLine} from 'react-icons/ri';
+import useForm from "../useForm";
+
+function WorkspaceForm(props) {
+    const {values, handleChange, handleSubmit} = useForm(props.handleSubmit);
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                placeholder="name"
+                onChange={handleChange}
+                className="form-control"
+                type="text"
+                name="name"/>
+            <button className="btn btn-primary p-1 m-1" type="submit">
+                <IconContext.Provider
+                    value={{
+                        size: "1.2rem",
+                        textAlign: "center"
+                    }}>
+                    <RiSave3Line></RiSave3Line>
+                </IconContext.Provider>
+            </button>
+            <button
+                className="btn btn-danger p-1 m-1"
+                type="button"
+                onClick={props.handleCancel}>
+                <IconContext.Provider
+                    value={{
+                        size: "1.2rem",
+                        textAlign: "center"
+                    }}>
+                    <RiCloseLine></RiCloseLine>
+                </IconContext.Provider>
+            </button>
+        </form>
+    );
+}
+
+export default WorkspaceForm;
--- src/components/Workspace/index.js
+++ src/components/Workspace/index.js
@@ -1,27 +1,171 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import CardDeck from 'react-bootstrap/CardDeck';
+// import WorkspaceItem from './WorkspaceItem';
+import WorkspaceForm from "./WorkspaceForm";
 import './styles.css';
-import WorkspaceItem from './WorkspaceItem'
+import { IconContext } from "react-icons";
+import { BsTrash, BsGrid1X2, BsPen } from 'react-icons/bs';
+import { WORKSPACE_SELECTED } from '../../../src/topics/workspaces';
+import {FcPlus} from 'react-icons/fc';
+import {motion} from 'framer-motion'; 
+import { API_ENDPOINT } from '../../../src/const';
+import { useHistory } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import PubSub from 'pubsub-js';
 import axios from 'axios';
 
-function getWorkspaces () {
-	axios
-		.get(`/documents/${document.id}`)
-		.then( response => {
-
-		})
-}
-
 function ViewWorkspace() {
+	const [workspaceList, setWorkspaceList] = useState({});
+	const [isAdding, setIsAdding] = useState(false);
+	const [isUpdating, setIsUpdating] = useState({status: false});
+	const history = useHistory();
+
+	const getWorkspaces = async () => {
+		let workspaces = await axios
+			.get(`${API_ENDPOINT}/workspaces/`);
+
+		return workspaces
+	}
+
+	const renderWorkspaces = () => {
+		getWorkspaces().then((workspaces) => {
+			setWorkspaceList(workspaces);
+		});
+	}
+
+	const handleDelete = async (e) => {
+		console.log("delete")
+		let workspaceId = e.currentTarget.id;
+		await axios
+			.delete(`${API_ENDPOINT}/workspaces/${workspaceId}`);
+
+		renderWorkspaces();
+		return true;
+	}
+
+	const handleWillUpdate = (e) => {
+		console.log("update")
+
+		let workspaceId = e.currentTarget.id;
+		setIsUpdating({status: true, id: workspaceId})
+	}
+
+	function handleSelect(props) {
+		history.push('/dashboard');
+		PubSub.subscribe(WORKSPACE_SELECTED, props);
+	}
+
+    const toggle = useCallback(() => {
+        setIsAdding(!isAdding)
+    }, [isAdding, setIsAdding]);
+
+    function handleCancelAdd() {
+        toggle();
+	}
+	
+	function handleCancelUpd(){
+		setIsUpdating({status: false})		
+	}
+
+    function addWorkspace() {
+        toggle();
+    }
+
+    async function handleSubmitAdd(values) {
+        await axios
+			.post(`${API_ENDPOINT}/workspaces`, values);
+		toggle();
+		renderWorkspaces();
+	}
+	
+	async function handleSubmitUpd(values) {
+        await axios
+			.put(`${API_ENDPOINT}/workspaces/${isUpdating.id}`, values);
+		setIsUpdating({status: false});
+		renderWorkspaces();
+    }
+
+	useEffect(() => {
+		renderWorkspaces();
+	}, [])
+
 	return (
 		<>
 			<header className='header'>
 				<h2>
-                    Select a workspace:
+					Select a workspace:
 				</h2>
 			</header>
 			<CardDeck>
-				<WorkspaceItem name="teste" />
+				{
+					workspaceList.data
+						? workspaceList.data.map((workspaceItem, index) => {
+							return (
+								<div className="workspace-item-container" key={index}>
+									{
+											(isUpdating.status && isUpdating.id == workspaceItem._id)
+												?   ""
+												:	<div className="icon-container"  >
+														<div id={workspaceItem._id} onClick={(e) => handleDelete(e)}>
+															<IconContext.Provider value={{ className: "delete-workspace-icon" }} >
+																<BsTrash />
+															</IconContext.Provider>
+														</div>
+														<div id={workspaceItem._id} onClick={(e) => handleWillUpdate(e)}>
+															<IconContext.Provider value={{ className: "update-workspace-icon" }} >
+																<BsPen />
+															</IconContext.Provider>
+														</div>
+													</div>
+									}
+									{/* <WorkspaceItem name={workspaceItem.name} /> */}
+										{
+											(isUpdating.status && isUpdating.id == workspaceItem._id)
+												?   <WorkspaceForm
+														handleSubmit={handleSubmitUpd}
+														handleCancel={handleCancelUpd} />
+
+												:	<Card className="workspace-card" bg="secondary" onClick={(e) => handleSelect(workspaceItem)}>
+														<Card.Header>
+															{workspaceItem.name}
+														</Card.Header>
+														<Card.Body>
+															<BsGrid1X2 size={100} />
+														</Card.Body>
+													</Card>
+										}
+								</div>
+							)
+						})
+						: "Loading..."
+				}
+				{/* <WorkspaceNew></WorkspaceNew> */}
+
+				<motion.div whileHover={{
+					scale: 1.1
+				}}>
+					<Card className="workspace-card" bg="dark">
+						<Card.Body className="new-workspace-card">
+							{
+								(isAdding == false)
+									? <div onClick={addWorkspace}>
+										<IconContext.Provider
+											value={{
+												size: "50px",
+												textAlign: "center"
+											}}>
+											<FcPlus />
+										</IconContext.Provider>
+									</div>
+
+									: <WorkspaceForm
+										handleSubmit={handleSubmitAdd}
+										handleCancel={handleCancelAdd} />
+							}
+						</Card.Body>
+					</Card>
+				</motion.div>
+
 			</CardDeck>
 		</>
 	);
